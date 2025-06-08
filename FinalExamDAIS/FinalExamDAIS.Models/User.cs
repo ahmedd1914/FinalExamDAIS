1f97403273ba0f3bed1cc454ed146d98ca4f7e7d
DESKTOP-IG0MKVO\Intern <metog2227@gmail.com>
2025-06-08 13:57:01 +0300

    Making the skeleton + login/register function.

---
+++ FinalExamDAIS/FinalExamDAIS.Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace FinalExamDAIS.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ FinalExamDAIS/FinalExamDAIS.Repository/Interfaces/User/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinalExamDAIS.Repository.Base;
+
+namespace FinalExamDAIS.Repository.Interfaces.User
+{
+    public interface IUserRepository : IBaseRepository<Models.User, UserFilter, UserUpdate>
+    {
+    }
+}
